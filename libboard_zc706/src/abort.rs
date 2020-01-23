c900f57be865c96564bca8139199674a36fa0a23
Astro <astro@spaceboyz.net>
2020-01-23 22:45:05 +0100

    libboard_zc706::abort: soft_reset on PrefetchAbort/DataAbort

--- libboard_zc706/src/abort.rs
+++ libboard_zc706/src/abort.rs
@@ -1,10 +1,12 @@
-use libboard_zynq::{println, stdio};
+use libboard_zynq::{println, slcr, stdio};
 
 #[no_mangle]
 pub unsafe extern "C" fn PrefetchAbort() {
     stdio::drop_uart();
 
     println!("PrefetchAbort");
+
+    slcr::RegisterBlock::unlocked(|slcr| slcr.soft_reset());
     loop {}
 }
 
@@ -13,5 +15,7 @@ pub unsafe extern "C" fn DataAbort() {
     stdio::drop_uart();
 
     println!("DataAbort");
+
+    slcr::RegisterBlock::unlocked(|slcr| slcr.soft_reset());
     loop {}
 }
