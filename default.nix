69093f4b477bf752053d995e13a9c9d484f03b7e
Astro <astro@spaceboyz.net>
2020-04-25 01:42:05 +0200

    default.nix: update cargoSha256

--- default.nix
+++ default.nix
@@ -49,7 +49,7 @@ let
     name = "${crate}";
     src = ./.;
     crateSubdir = crate;
-    cargoSha256 = "02k72zj6qfbw9y69w3r9a1b1dg9pvkc7cl511hcv4w9rbnn3jlld";
+    cargoSha256 = "0wqsxbcphcf240mqfglckl0lz82f19g1jlcf3xk73aflpyxk5pmm";
     cargoFeatures = features;
     doCheck = false;
   };
