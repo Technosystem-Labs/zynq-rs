3bbd1513fbff3a900af1dc43caab5ab4db2c83a8
pca006132 <john.lck40@gmail.com>
2020-08-03 14:08:36 +0800

    build.sh: specify build experiments
    Otherwise we cannot turn off the default feature for libsupport_zynq.
    https://github.com/rust-lang/cargo/issues/8366#issuecomment-644995218

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-nix-shell --command "cargo xbuild --release" && scp -P 2204 -C target/armv7-none-eabihf/release/zc706-experiments $1@nixbld.m-labs.hk:/home/$1/zc706/zc706.elf
+nix-shell --command "cargo xbuild --release -p experiments" && scp -P 2204 -C target/armv7-none-eabihf/release/zc706-experiments $1@nixbld.m-labs.hk:/home/$1/zc706/zc706.elf
