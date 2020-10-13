27d310a93722c07b1c96091d5b22cbcdd6547f10
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-10-13 18:25:39 +0800

    update Rust
    Needed for compatibility with recent cargo-xbuild.

--- nix/rust-platform.nix
+++ nix/rust-platform.nix
@@ -4,8 +4,8 @@ let
   rustcSrc = pkgs.fetchgit {
     url = "https://github.com/rust-lang/rust.git";
     # sync with git_commit_hash from pkg.rust in channel-rust-nightly.toml
-    rev = "5ef299eb9805b4c86b227b718b39084e8bf24454";
-    sha256 = "0gc9hmb1sfkaf3ba8fsynl1n6bs8nk65hbhhx7ss89dfkrsxrn0x";
+    rev = "8dae8cdcc8fa879cea6a4bbbfa5b32e97be4c306";
+    sha256 = "1c9wdwqvskchiqkxqnl516z2yajs2nbs31y4j6m2jz945dj8x4xc";
     fetchSubmodules = true;
   };
   rustManifest = ./channel-rust-nightly.toml;
