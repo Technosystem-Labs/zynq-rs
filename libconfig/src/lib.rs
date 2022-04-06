56c27e98e4fc2bd719d26abaa98a7628c395b9c0
mwojcik <mw@m-labs.hk>
2022-04-06 16:17:57 +0800

    config: add "fat_lfn" feature

--- libconfig/src/lib.rs
+++ libconfig/src/lib.rs
@@ -164,7 +164,7 @@ impl Config {
                 f.seek(SeekFrom::End(0))?;
                 write!(f, "{}={}\n", key, String::from_utf8(value).unwrap())?;
             } else {
-                let mut dir = root_dir.create_dir("/CONFIG")?;
+                let dir = root_dir.create_dir("/CONFIG")?;
                 let mut f = dir.create_file(&[key, ".BIN"].concat())?;
                 f.write_all(&value)?;
             }
