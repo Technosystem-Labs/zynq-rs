20975fc812d6b4e28b9fe6011344373054581c94
Sebastien Bourdeauducq <sb@m-labs.hk>
2025-11-18 18:21:35 +0800

    flake: format

--- flake.nix
+++ flake.nix
@@ -11,21 +11,30 @@
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, naersk }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; overlays = [ (import rust-overlay) crosspkgs-overlay ]; };
-      
-      rust = pkgs.rust-bin.nightly."2025-03-28".default.override {
-        extensions = [ "rust-src" ];
-        targets = [ ];
-      };
-      naerskLib = pkgs.callPackage naersk {
-        rustc = rust;
-        cargo = rust;
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    naersk,
+  }: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [(import rust-overlay) crosspkgs-overlay];
+    };
 
-      crosspkgs-overlay = (self: super: {
-        pkgsCross = super.pkgsCross // {
+    rust = pkgs.rust-bin.nightly."2025-03-28".default.override {
+      extensions = ["rust-src"];
+      targets = [];
+    };
+    naerskLib = pkgs.callPackage naersk {
+      rustc = rust;
+      cargo = rust;
+    };
+
+    crosspkgs-overlay = self: super: {
+      pkgsCross =
+        super.pkgsCross
+        // {
           zynq-baremetal = import super.path {
             system = "x86_64-linux";
             crossSystem = {
@@ -36,33 +45,32 @@
             };
           };
         };
-      });
+    };
 
-      mkbootimage = pkgs.stdenv.mkDerivation {
-        pname = "mkbootimage";
-        version = "2.3dev";
+    mkbootimage = pkgs.stdenv.mkDerivation {
+      pname = "mkbootimage";
+      version = "2.3dev";
 
-        src = pkgs.fetchFromGitHub {
-          owner = "antmicro";
-          repo = "zynq-mkbootimage";
-          rev = "872363ce32c249f8278cf107bc6d3bdeb38d849f";
-          sha256 = "sha256-5FPyAhUWZDwHbqmp9J2ZXTmjaXPz+dzrJMolaNwADHs=";
-        };
-
-        propagatedBuildInputs = [ pkgs.libelf pkgs.pcre ];
-        patchPhase =
-        ''
-          substituteInPlace Makefile --replace "git rev-parse --short HEAD" "echo nix"
-        '';
-        installPhase =
-        ''
-          mkdir -p $out/bin
-          cp mkbootimage $out/bin
-        '';
-        hardeningDisable = [ "fortify" ];
+      src = pkgs.fetchFromGitHub {
+        owner = "antmicro";
+        repo = "zynq-mkbootimage";
+        rev = "872363ce32c249f8278cf107bc6d3bdeb38d849f";
+        sha256 = "sha256-5FPyAhUWZDwHbqmp9J2ZXTmjaXPz+dzrJMolaNwADHs=";
       };
 
-      fsbl = { board ? "zc706" }: pkgs.stdenv.mkDerivation {
+      propagatedBuildInputs = [pkgs.libelf pkgs.pcre];
+      patchPhase = ''
+        substituteInPlace Makefile --replace "git rev-parse --short HEAD" "echo nix"
+      '';
+      installPhase = ''
+        mkdir -p $out/bin
+        cp mkbootimage $out/bin
+      '';
+      hardeningDisable = ["fortify"];
+    };
+
+    fsbl = {board ? "zc706"}:
+      pkgs.stdenv.mkDerivation {
         name = "${board}-fsbl";
         src = pkgs.fetchFromGitHub {
           owner = "Xilinx";
@@ -77,7 +85,7 @@
 
         NIX_CFLAGS_COMPILE = "-DFSBL_DEBUG_INFO -g -no-pie";
         NIX_LDFLAGS = "-no-pie";
-        
+
         patchPhase = ''
           patchShebangs lib/sw_apps/zynq_fsbl/misc/copy_bsp.sh
 
@@ -98,18 +106,21 @@
         dontFixup = true;
       };
 
-      build-crate = name: crate: features: naerskLib.buildPackage rec {
+    build-crate = name: crate: features:
+      naerskLib.buildPackage rec {
         name = "${crate}";
         src = ./.;
         additionalCargoLock = "${rust}/lib/rustlib/src/rust/library/Cargo.lock";
-        nativeBuildInputs = [ pkgs.llvmPackages_20.clang-unwrapped ];
+        nativeBuildInputs = [pkgs.llvmPackages_20.clang-unwrapped];
         singleStep = true;
         release = true;
-        cargoBuildOptions = options: options ++ [
-          "-p ${crate}"
-          "--no-default-features"
-          "--features=${features}"
-        ];
+        cargoBuildOptions = options:
+          options
+          ++ [
+            "-p ${crate}"
+            "--no-default-features"
+            "--features=${features}"
+          ];
         overrideMain = _: {
           installPhase = ''
             mkdir -p $out $out/nix-support
@@ -120,59 +131,68 @@
         };
       };
 
-      targetCrates = target: {
-        "${target}-experiments" = build-crate "${target}-experiments" "experiments" "target_${target}";
-        "${target}-szl" = build-crate "${target}-szl" "szl" "target_${target}";
-      };
-      targets = ["zc706" "coraz7" "redpitaya" "kasli_soc" "ebaz4205"];
-      allTargetCrates = (builtins.foldl' (results: target:
-        results // targetCrates target
-      ) {} targets);
-      
-      szl = pkgs.runCommand "szl" {} (builtins.foldl' (commands: target:
-        let
+    targetCrates = target: {
+      "${target}-experiments" = build-crate "${target}-experiments" "experiments" "target_${target}";
+      "${target}-szl" = build-crate "${target}-szl" "szl" "target_${target}";
+    };
+    targets = ["zc706" "coraz7" "redpitaya" "kasli_soc" "ebaz4205"];
+    allTargetCrates =
+      builtins.foldl' (
+        results: target:
+          results // targetCrates target
+      ) {}
+      targets;
+
+    szl = pkgs.runCommand "szl" {} (builtins.foldl' (
+        commands: target: let
           szlResult = builtins.getAttr "${target}-szl" allTargetCrates;
         in
           commands + "ln -s ${szlResult}/szl.elf $out/szl-${target}.elf\n"
-      ) "mkdir $out\n" targets);
+      ) "mkdir $out\n"
+      targets);
 
-      fmt-check = pkgs.stdenvNoCC.mkDerivation {
-        name = "fmt-check";
+    fmt-check = pkgs.stdenvNoCC.mkDerivation {
+      name = "fmt-check";
 
-        src = ./.;
+      src = ./.;
 
-        nativeBuildInputs = [ rust ];
+      nativeBuildInputs = [rust];
 
-        phases = [ "unpackPhase" "buildPhase" ];
+      phases = ["unpackPhase" "buildPhase"];
 
-        buildPhase =
-          ''
-          cargo fmt -- --check
-          touch $out
-          '';
-      };
-    in rec {
-      packages.x86_64-linux = { 
-        inherit szl mkbootimage fmt-check;
-        zc706-fsbl = fsbl { board = "zc706"; };
-      } // allTargetCrates ;
-
-      hydraJobs = packages.x86_64-linux;
-
-      inherit rust naerskLib;
-
-      devShell.x86_64-linux = pkgs.mkShell {
-        name = "zynq-rs-dev-shell";
-        buildInputs = [
-          rust
-          pkgs.cargo-xbuild
-          mkbootimage
-
-          pkgs.openocd pkgs.gdb
-          pkgs.openssh pkgs.rsync
-          pkgs.llvmPackages_20.clang-unwrapped
-          (pkgs.python3.withPackages(ps: [ ps.pyftdi ]))
-        ];
-      };
+      buildPhase = ''
+        cargo fmt -- --check
+        touch $out
+      '';
     };
+  in rec {
+    packages.x86_64-linux =
+      {
+        inherit szl mkbootimage fmt-check;
+        zc706-fsbl = fsbl {board = "zc706";};
+      }
+      // allTargetCrates;
+
+    hydraJobs = packages.x86_64-linux;
+
+    inherit rust naerskLib;
+
+    formatter.x86_64-linux = pkgs.alejandra;
+
+    devShell.x86_64-linux = pkgs.mkShell {
+      name = "zynq-rs-dev-shell";
+      buildInputs = [
+        rust
+        pkgs.cargo-xbuild
+        mkbootimage
+
+        pkgs.openocd
+        pkgs.gdb
+        pkgs.openssh
+        pkgs.rsync
+        pkgs.llvmPackages_20.clang-unwrapped
+        (pkgs.python3.withPackages (ps: [ps.pyftdi]))
+      ];
+    };
+  };
 }
